0ce984894e6d902d21f2f3322332abceac4d3ad2
ysknsid25 <kengo071225@gmail.com>
2022-11-09 08:07:18 +0900

    Theories -> Enclosed
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/test/ParameterizedTest.java
+++ src/test/ParameterizedTest.java
@@ -1,36 +1,41 @@
 package test;
 
+import org.junit.experimental.runners.Enclosed;
 import org.junit.experimental.theories.DataPoint;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
-@RunWith(Theories.class)
+@RunWith(Enclosed.class)
 public class ParameterizedTest {
-  @DataPoint
-  public static int INT_PARAM_1 = 3;
+  @RunWith(Theories.class)
+  public static class IntParameterizedTest {
+    @DataPoint
+    public static int INT_PARAM_1 = 3;
 
-  @DataPoint
-  public static int INT_PARAM_2 = 4;
+    @DataPoint
+    public static int INT_PARAM_2 = 4;
 
-  @DataPoint
-  public static String STR_PARAM_1 = "Hello";
+    @Theory
+    public void hasParameterMethodTest(int param) throws Exception {
+      System.out.println("Parameter is " + param);
+    }
 
-  @DataPoint
-  public static String STR_PARAM_2 = "World";
-
-  public ParameterizedTest() {
-    System.out.println("initialized");
   }
 
-  @Theory
-  public void hasParameterMethodTest(int param) throws Exception {
-    System.out.println("Parameter is " + param);
-  }
+  @RunWith(Theories.class)
+  public static class StringParameterizedTest {
+    @DataPoint
+    public static String STR_PARAM_1 = "Hello";
 
-  @Theory
-  public void hasStringParameterMethodTest(String param) throws Exception {
-    System.out.println("Parameter is " + param);
+    @DataPoint
+    public static String STR_PARAM_2 = "World";
+
+    @Theory
+    public void hasStringParameterMethodTest(String param) throws Exception {
+      System.out.println("Parameter is " + param);
+    }
+
   }
 
 }
