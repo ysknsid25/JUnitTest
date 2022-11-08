bcabc1cd2602643898d4bb007edfc5fcb03e7434
ysknsid25 <kengo071225@gmail.com>
2022-11-09 08:05:11 +0900

    Theories Test
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

---
+++ src/test/ParameterizedTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Theories.class)
+public class ParameterizedTest {
+  @DataPoint
+  public static int INT_PARAM_1 = 3;
+
+  @DataPoint
+  public static int INT_PARAM_2 = 4;
+
+  @DataPoint
+  public static String STR_PARAM_1 = "Hello";
+
+  @DataPoint
+  public static String STR_PARAM_2 = "World";
+
+  public ParameterizedTest() {
+    System.out.println("initialized");
+  }
+
+  @Theory
+  public void hasParameterMethodTest(int param) throws Exception {
+    System.out.println("Parameter is " + param);
+  }
+
+  @Theory
+  public void hasStringParameterMethodTest(String param) throws Exception {
+    System.out.println("Parameter is " + param);
+  }
+
+}
