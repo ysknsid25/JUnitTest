a8163c689b338f7ba8e5ac96d92f165d6a7e6006
ysknsid25 <kengo071225@gmail.com>
2022-11-09 08:09:03 +0900

    テストメソッドに複数の引数が定義されている場合
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/test/ParameterizedTest.java
+++ src/test/ParameterizedTest.java
@@ -1,41 +1,31 @@
 package test;
 
-import org.junit.experimental.runners.Enclosed;
 import org.junit.experimental.theories.DataPoint;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
-@RunWith(Enclosed.class)
+@RunWith(Theories.class)
 public class ParameterizedTest {
-  @RunWith(Theories.class)
-  public static class IntParameterizedTest {
-    @DataPoint
-    public static int INT_PARAM_1 = 3;
+  @DataPoint
+  public static int INT_PARAM_1 = 3;
 
-    @DataPoint
-    public static int INT_PARAM_2 = 4;
+  @DataPoint
+  public static int INT_PARAM_2 = 4;
 
-    @Theory
-    public void hasParameterMethodTest(int param) throws Exception {
-      System.out.println("Parameter is " + param);
-    }
+  @DataPoint
+  public static String STR_PARAM_1 = "Hello";
 
+  @DataPoint
+  public static String STR_PARAM_2 = "World";
+
+  public ParameterizedTest() {
+    System.out.println("initialized");
   }
 
-  @RunWith(Theories.class)
-  public static class StringParameterizedTest {
-    @DataPoint
-    public static String STR_PARAM_1 = "Hello";
-
-    @DataPoint
-    public static String STR_PARAM_2 = "World";
-
-    @Theory
-    public void hasStringParameterMethodTest(String param) throws Exception {
-      System.out.println("Parameter is " + param);
-    }
-
+  @Theory
+  public void hasParameterMethodTest(int param, String strparam) throws Exception {
+    System.out.println("Parameter is " + param + "," + strparam);
   }
 
 }
