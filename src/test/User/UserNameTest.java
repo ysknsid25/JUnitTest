7776b191578c26e3761fc25e2b8f551c35eda172
ysknsid25 <kengo071225@gmail.com>
2022-11-10 19:52:11 +0900

    Refactor
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- src/test/User/UserNameTest.java
+++ src/test/User/UserNameTest.java
@@ -5,9 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
-import org.junit.runner.RunWith;
 
 import main.User.UserName;
 
